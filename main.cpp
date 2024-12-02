25b624761260121f801b8318605961182367cf11
scott-wade <stwade@bu.edu>
2024-12-02 00:25:29 +0000

    added hardware folder

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include "main.h"
 #include "debug_mort.h"
 #include "nucleo_led.h"
-#include "hardware_stm_gpio.h"
-#include "hardware_stm_timer3.h"
-#include "hardware_stm_interrupt.h"
+#include "hardware/hardware_stm_gpio.h"
+#include "hardware/hardware_stm_timer3.h"
+#include "hardware/hardware_stm_interrupt.h"
 
 int main (void)
 {
